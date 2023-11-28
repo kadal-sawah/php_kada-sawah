caa59c360713224b57cd4f3876227290eb3f1048
kadal-sawah <riz.idn.dev@gmail.com>
2023-11-28 15:27:13 +0700

    added index

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document Tera</title>
+</head>
+<body>
+    
+    <a href="/tera/soal1a.php">Ke Soal 1</a></br>
+    <a href="/tera/soal3a.php">Ke Soal 2</a>
+
+</body>
+</html>
